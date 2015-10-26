56ab55f8c75fd20115b117d7714b387612ff0691
yiminghe <yiminghe@gmail.com>
2015-10-26 18:21:41 +0800

    use falcor-koa-router

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var path = require('path');
 var staticDir = path.join(__dirname, 'static');
 var app = require('koa')();
-var FalcorRouter = require('falcor-router');
-var falcorKoa = require('./server/falcor-koa');
+var falcorKoaRouter = require('falcor-koa-router');
 var mount = require('koa-mount');
+
+function wait(ms) {
+  return function (done) {
+    setTimeout(done, ms);
+  }
+}
+
 var todos = [
   {
     id: '1',
@@ -41,27 +47,25 @@ todos.forEach(function (t) {
   todosMap[t.id] = t;
 });
 
-var BaseRouter = FalcorRouter.createClass([
+app.use(require('koa-body')());
+app.use(mount('/model.json', falcorKoaRouter.routes([
   {
     route: 'todoById[{integers}][{keys}]',
-    set(args) {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          var ret = [];
-          var todoById = args.todoById;
-          for (var id in todoById) {
-            var target = todoById[id];
-            for (var p in target) {
-              todosMap[id][p] = todoById[id][p];
-              ret.push({
-                path: ['todoById', id, p],
-                value: todoById[id][p]
-              });
-            }
-          }
-          resolve(ret);
-        }, 100);
-      });
+    set: function* (args) {
+      yield wait(100);
+      var ret = [];
+      var todoById = args.todoById;
+      for (var id in todoById) {
+        var target = todoById[id];
+        for (var p in target) {
+          todosMap[id][p] = todoById[id][p];
+          ret.push({
+            path: ['todoById', id, p],
+            value: todoById[id][p]
+          });
+        }
+      }
+      return ret;
     }
   },
   {
@@ -79,148 +83,118 @@ var BaseRouter = FalcorRouter.createClass([
   {
     route: 'todoById[{integers:ids}][{keys:ps}]',
     get(args) {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          var ret = [];
-          var ids = args.ids;
-          var ps = args.ps;
-          ps.forEach(function (p) {
-            ids.forEach(function (id) {
-              ret.push({
-                path: ['todoById', id, p],
-                value: todosMap[id][p]
-              });
-            });
+      var ret = [];
+      var ids = args.ids;
+      var ps = args.ps;
+      ps.forEach(function (p) {
+        ids.forEach(function (id) {
+          ret.push({
+            path: ['todoById', id, p],
+            value: todosMap[id][p]
           });
-          resolve(ret);
-        }, 100);
+        });
       });
+      return ret;
     }
   },
   {
     route: 'todo.top[{ranges:r}]',
     get(args) {
       var ctx = this.ctx;
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          var sortedTodos = todos.concat().sort(function (t1, t2) {
-            return t2.priority - t1.priority;
-          }).slice(args.r[0].from, args.r[0].to + 1);
-          var index = args.r[0].from;
-          var refs = sortedTodos.map(function (t, i) {
-            return {
-              path: ['todo', 'top', index++],
-              value: {
-                $type: 'ref',
-                value: ['todoById', t.id]
-              }
-            }
-          });
-          var topTodoMaps = sortedTodos.map(function (t) {
-            return ({
-              path: ['todoById', t.id],
-              value: todosMap[t.id]
-            })
-          });
-          //console.log('top',topTodoMaps)
 
-          resolve(topTodoMaps.concat(refs));
-        }, 100);
+      var sortedTodos = todos.concat().sort(function (t1, t2) {
+        return t2.priority - t1.priority;
+      }).slice(args.r[0].from, args.r[0].to + 1);
+      var index = args.r[0].from;
+      var refs = sortedTodos.map(function (t, i) {
+        return {
+          path: ['todo', 'top', index++],
+          value: {
+            $type: 'ref',
+            value: ['todoById', t.id]
+          }
+        };
       });
+      var topTodoMaps = sortedTodos.map(function (t) {
+        return ({
+          path: ['todoById', t.id],
+          value: todosMap[t.id]
+        })
+      });
+      //console.log('top',topTodoMaps)
+
+      return (topTodoMaps.concat(refs));
     }
   },
   {
     route: 'todo.bottom[{ranges:r}]',
-    get(args) {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          var sortedTodos = todos.concat().sort(function (t1, t2) {
-            return t1.priority - t2.priority;
-          }).slice(args.r[0].from, args.r[0].to + 1);
-          var index = args.r[0].from;
-          var refs = sortedTodos.map(function (t, i) {
-            return {
-              path: ['todo', 'bottom', index++],
-              value: {
-                $type: 'ref',
-                value: ['todoById', t.id]
-              }
-            }
-          });
-          var topTodoMaps = sortedTodos.map(function (t) {
-            return ({
-              path: ['todoById', t.id],
-              value: todosMap[t.id]
-            })
-          });
-          //console.log('bottom',topTodoMaps)
-          resolve(topTodoMaps.concat(refs));
-        }, 100);
+    get: function *(args) {
+      yield wait(100);
+      var sortedTodos = todos.concat().sort(function (t1, t2) {
+        return t1.priority - t2.priority;
+      }).slice(args.r[0].from, args.r[0].to + 1);
+      var index = args.r[0].from;
+      var refs = sortedTodos.map(function (t, i) {
+        return {
+          path: ['todo', 'bottom', index++],
+          value: {
+            $type: 'ref',
+            value: ['todoById', t.id]
+          }
+        }
       });
+      var topTodoMaps = sortedTodos.map(function (t) {
+        return ({
+          path: ['todoById', t.id],
+          value: todosMap[t.id]
+        })
+      });
+      //console.log('bottom',topTodoMaps)
+      return (topTodoMaps.concat(refs));
     }
   },
   {
     route: 'todo.length',
-    get(args) {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve({
-            path: ['todo', 'length'],
-            value: todos.length
-          });
-        }, 100);
+    get() {
+      return ({
+        path: ['todo', 'length'],
+        value: todos.length
       });
     }
   },
   {
     route: 'todo.default[{ranges:r}]',
     get(args) {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          var refs = todos.map(function (t, i) {
-            if (i > args.r[0].to || i < args.r[0].from) {
-              return null;
-            }
-            return {
-              path: ['todo', 'default', i],
-              value: {
-                $type: 'ref',
-                value: ['todoById', t.id]
-              }
-            };
-          }).filter(function (v) {
-            return !!v
-          });
-          var maps = todos.map(function (t, i) {
-            if (i > args.r[0].to || i < args.r[0].from) {
-              return null;
-            }
-            return {
-              path: ['todoById', t.id],
-              value: todosMap[t.id]
-            };
-          }).filter(function (v) {
-            return !!v
-          });
-          //console.log('default',maps)
-          resolve(maps.concat(refs));
-        }, 100);
+      var refs = todos.map(function (t, i) {
+        if (i > args.r[0].to || i < args.r[0].from) {
+          return null;
+        }
+        return {
+          path: ['todo', 'default', i],
+          value: {
+            $type: 'ref',
+            value: ['todoById', t.id]
+          }
+        };
+      }).filter(function (v) {
+        return !!v
       });
+      var maps = todos.map(function (t, i) {
+        if (i > args.r[0].to || i < args.r[0].from) {
+          return null;
+        }
+        return {
+          path: ['todoById', t.id],
+          value: todosMap[t.id]
+        };
+      }).filter(function (v) {
+        return !!v
+      });
+      //console.log('default',maps)
+      return (maps.concat(refs));
     }
   }
-]);
-
-var MyRouter = function (ctx) {
-  BaseRouter.call(this);
-  this.ctx = ctx;
-};
-
-MyRouter.prototype = Object.create(BaseRouter.prototype);
-
-app.use(require('koa-body')());
-app.use(mount('/model.json', falcorKoa.dataSourceRoute(function *(next) {
-  return new MyRouter(this);
-})));
+])));
 
 require('rc-server')(app);
 app.listen(9001);
